6da300e34324da9ea391a87c45ce5ab3bb2d2a33
nagato-gamed <belalgamed001@gmail.com>
2026-09-24 23:12:36 +0300

    dsd

--- jonas code/starter/script.js
+++ jonas code/starter/script.js
@@ -13,3 +13,33 @@ function Human(firstname,birthYear){
  console.log(Human.prototype)
  
 
+'use strict';
+// function Human(firstname,birthYear){
+//     this.firstname=firstname
+//     this.birthYear=birthYear
+
+   
+// }
+//  const belal= new Human('belal',2006)
+//  const soli = new Human('solyman',2005)
+ 
+//  Human.prototype.calcage=function(){
+//     this.age= 2026- this.birthYear
+//  }
+//  soli.calcage()
+//  console.log(Human.prototype)
+//  console.log(soli)
+function Itiswhatitis(make,speed){
+    this.speed=speed
+    this.make=make
+}
+
+Itiswhatitis.prototype.accelerate=function(){this.speed=this.speed+10}
+Itiswhatitis.prototype.brake=function(){this.speed=this.speed-5}
+const car1=new Itiswhatitis('BMW',120)
+const car2=new Itiswhatitis('Mercedes',95)
+ car1.brake()
+ car1.brake()
+ car2.accelerate()
+ car2.accelerate()
+console.log(car1,car2)
