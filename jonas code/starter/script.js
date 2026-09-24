8556902ce2cd2c9984ae07c494d4819c2b0292c9
nagato-gamed <belalgamed001@gmail.com>
2026-09-24 22:59:30 +0300

    we start the oop yay

---
+++ jonas code/starter/script.js
@@ -0,0 +1,15 @@
+'use strict';
+function Human(firstname,birthYear){
+    this.firstname=firstname
+    this.birthYear=birthYear
+
+   
+}
+ const belal= new Human('belal',2006)
+ console.log(belal)
+ const soli = new Human('solyman',2005)
+ Human.prototype.calc
+ console.log(soli.prototype)
+ console.log(Human.prototype)
+ 
+
